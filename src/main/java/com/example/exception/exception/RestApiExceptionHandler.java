940baf77169b1498fde163b0591b685ed0bf8b4f
fullm <matoma1_@naver.com>
2024-04-12 15:47:28 +0900

    @ExceptionHandler 사용해보기

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
-@RestControllerAdvice // rest api를 사용하는 곳에 예외가 일어나는 것을 감지
+@RestControllerAdvice // rest api를 사용하는 곳에 예외가 일어나는 것을 감지, 모든 컨트롤러에서 나오는 예외를 잡아줌
 public class RestApiExceptionHandler {
 
     //감지후 어떻게 이 api를 인식할 수 있는지 메소드 작성
@@ -23,3 +23,5 @@ public class RestApiExceptionHandler {
         return ResponseEntity.status(200).build();
     }
 }
+// 이 컨트롤러는 두가지 예외를 잡는다. indexoutofbound, exception을 상속받는 모든 예외
+// RestControllerAdvice 모든 예외를 잡아주는 글로벌한 예외 컨트롤러이다.
