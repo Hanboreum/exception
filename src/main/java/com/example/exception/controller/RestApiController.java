029eca11aea9d78f5e42aae682757473b6a7fc43
fullm <matoma1_@naver.com>
2024-04-12 14:53:54 +0900

    Exception 발생 시켜보기

--- src/main/java/com/example/exception/controller/RestApiController.java
+++ src/main/java/com/example/exception/controller/RestApiController.java
@@ -1,4 +1,19 @@
 package com.example.exception.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+    //http://localhost:8080/api 실행시 500error (= 시스템 서버 에러)
+    @GetMapping(path = "")
+    public void hello(){
+
+        throw new RuntimeException("run time exception call");
+    }
 }
