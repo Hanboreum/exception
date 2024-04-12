8ee72be3725a42cde98b4d6ecf4b024dcc13ede6
fullm <matoma1_@naver.com>
2024-04-12 14:58:53 +0900

    IndexOutOfBoundsException 발생 시켜보기

--- src/main/java/com/example/exception/controller/RestApiController.java
+++ src/main/java/com/example/exception/controller/RestApiController.java
@@ -1,11 +1,12 @@
 package com.example.exception.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
@@ -13,7 +14,9 @@ public class RestApiController {
     //http://localhost:8080/api 실행시 500error (= 시스템 서버 에러)
     @GetMapping(path = "")
     public void hello(){
-
-        throw new RuntimeException("run time exception call");
+        var list = List.of("hello");
+        var element = list.get(1);
+        log.info("element:  {}", element); //500 error": "Internal Server Error",
     }
 }
+//throw new RuntimeException("run time exception call");
