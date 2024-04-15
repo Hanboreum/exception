333ff00d420e7c5f226bd9573e30a6d2052a8c1d
fullm <matoma1_@naver.com>
2024-04-15 17:29:50 +0900

    snake case로 답변 하기 위한  @JsonNaming

--- src/main/java/com/example/exception/model/Api.java
+++ src/main/java/com/example/exception/model/Api.java
@@ -1,5 +1,8 @@
 package com.example.exception.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class) //해당 클래스가 snake case로 답변이 내려갈 수 있게
 public class Api <T>{
 
     private String resultCode;
