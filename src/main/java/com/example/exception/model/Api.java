53a9120ea3323da7f8677e477fa2cb0a5a84888b
fullm <matoma1_@naver.com>
2024-04-15 17:33:52 +0900

    @Builder 빌더 패턴을 위한 어노테이션 추가

--- src/main/java/com/example/exception/model/Api.java
+++ src/main/java/com/example/exception/model/Api.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder //객체 생성시 빌더 패턴 사용 할 것
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class) //해당 클래스가 snake case로 답변이 내려갈 수 있게
 public class Api <T>{
 
