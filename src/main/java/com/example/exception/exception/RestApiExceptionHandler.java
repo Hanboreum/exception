5382e25c05bdfb4db261fc8efd4c871d31416df8
fullm <matoma1_@naver.com>
2024-04-16 15:12:27 +0900

    @Order(1) 통해 예외처리 순서 지정

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.exception.controller.RestApiBController;
 import com.example.exception.controller.RestApiController;
 import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,7 @@ import java.util.NoSuchElementException;
 //basePackages : 지정된 경로의 예외까지 잡겠다.
 @RestControllerAdvice(basePackageClasses = {RestApiController.class, RestApiBController.class})
 //해당 컨트롤러 어드바이스는 해당 클래스에서 발생하는 것 만 잡아주겠다. 배열 설정 가능. 많은 컨트롤러 지정 가능
+@Order(1) //globalhandler보다 먼저 처리 되어야하므로 순서 지정
 public class RestApiExceptionHandler {
 
     //감지후 어떻게 이 api를 인식할 수 있는지 메소드 작성
