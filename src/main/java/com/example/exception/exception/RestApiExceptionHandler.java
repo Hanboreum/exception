053e60942fa88190334e64589eddc0752a10ad3a
fullm <matoma1_@naver.com>
2024-04-16 15:35:56 +0900

    Rest, global 순서 테스트

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -20,15 +20,23 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice(basePackageClasses = {RestApiController.class, RestApiBController.class})
 //해당 컨트롤러 어드바이스는 해당 클래스에서 발생하는 것 만 잡아주겠다. 배열 설정 가능. 많은 컨트롤러 지정 가능
 @Order(1) //globalhandler보다 먼저 처리 되어야하므로 순서 지정
+//마이너스 -에 가까울 수록 마지막에 처리 된다.
 public class RestApiExceptionHandler {
+    @ExceptionHandler(value = { Exception.class}) //예측하지 못한 모든 것 잡아주므로 최상위 클래스 Exception.class
+    public ResponseEntity<Api> exception (Exception e){
+        log.error("" ,e);
+
+        var response = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .resultMessage(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
 
     //감지후 어떻게 이 api를 인식할 수 있는지 메소드 작성
-    @ExceptionHandler(value ={Exception.class}) //예외를 잡아주는, 어떤 예외를 잡을 것인지 설정
-    public ResponseEntity exception( Exception e){
-       log.error("RestApiExceptionHandler", e);
-
-       return ResponseEntity.status(200).build();
-    }
 
     //IndexOutOfBoundsException에 대한 예외만 잡음
     @ExceptionHandler(value = { IndexOutOfBoundsException.class })
@@ -54,6 +62,14 @@ public class RestApiExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(response); //response entity로 한 번 감싼 api
     }
+    //이게 있어서 runtime에러가 안됐다. 모든에러를 잡나보다
+   /* @ExceptionHandler(value ={Exception.class}) //예외를 잡아주는, 어떤 예외를 잡을 것인지 설정
+    public ResponseEntity exception( Exception e){
+       log.error("RestApiExceptionHandler", e);
+
+       return ResponseEntity.status(200).build();
+    }*/
+
 }
 // 이 컨트롤러는 두가지 예외를 잡는다. indexoutofbound, exception을 상속받는 모든 예외
 // RestControllerAdvice 모든 예외를 잡아주는 글로벌한 예외 컨트롤러이다.
